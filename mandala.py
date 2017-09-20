20eef03812dd15c4411bfb2a85b64461dad78118
xmduhan <xmduhan@gmail.com>
2017-09-20 10:54:38 +0800

    ...

--- mandala.py
+++ mandala.py
@@ -1,9 +1,39 @@
 #!/usr/bin/env python
 # encoding: utf-8
+from prompt_toolkit import prompt as _prompt
+from prompt_toolkit.history import InMemoryHistory
+
+history = InMemoryHistory()
+
+
+def prompt(text):
+    return _prompt(text, history=history)
+
+
+def hfeed():
+    """ """
+    print u'hfeed'
+
+
+def quit():
+    print u'再见!'
+    exit()
+
+
+commands = {
+    'hfeed': hfeed,
+    'quit': quit,
+}
 
 
 def main():
     """ """
+    while True:
+        cmd = prompt(u'mandala>')
+        if cmd in commands:
+            commands[cmd]()
+        else:
+            print u'无效命令'
 
 
 if __name__ == "__main__":
